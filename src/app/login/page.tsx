e0da06d4dd0ec368e42d8ead59d9d761f8444e17
Irvan Junaidi <49930719+irvanswan@users.noreply.github.com>
2025-07-31 00:06:49 +0700

    feat(login): irvan | view login page (#3)
    Co-authored-by: @IrvanJ <irvan.junaidi@phincon.com>

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import dynamic from 'next/dynamic';
 import Styles from '@/components/organisms/_login/login.module.scss';
+import { LoginProvider } from '@/components/organisms/_login/LoginContext';
 
 const Banner = dynamic(() => import('@/components/organisms/_login/Banner'));
 const Form = dynamic(() => import('@/components/organisms/_login/Form'));
@@ -11,9 +12,11 @@ export const metadata = {
 
 export default function Login() {
 	return (
-		<div className={`${Styles.Login} flex-col md:flex-row`}>
-			<Banner />
-			<Form />
-		</div>
+		<LoginProvider>
+			<div className={`${Styles.Login} flex-col md:flex-row`}>
+				<Banner />
+				<Form />
+			</div>
+		</LoginProvider>
 	)
 }
